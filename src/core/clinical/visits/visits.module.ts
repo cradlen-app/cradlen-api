a60334788377a198e36d6ec5a778329326e80aa8
ibrahem abodeif <ibrahem.mohamed191999@gmail.com>
2026-05-16 13:10:28 +0300

    feat(visits): add AuthorizationModule to visits module imports

--- src/core/clinical/visits/visits.module.ts
+++ src/core/clinical/visits/visits.module.ts
@@ -3,9 +3,10 @@ import { VisitsController } from './visits.controller';
 import { VisitsService } from './visits.service';
 import { EncounterMutationGuard } from './encounter-mutation.guard';
 import { ValidatorModule } from '@builder/validator/validator.module.js';
+import { AuthorizationModule } from '@core/auth/authorization/authorization.module.js';
 
 @Module({
-  imports: [ValidatorModule],
+  imports: [ValidatorModule, AuthorizationModule],
   controllers: [VisitsController],
   providers: [VisitsService, EncounterMutationGuard],
   exports: [EncounterMutationGuard],
